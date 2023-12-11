c4d05802140804e356beeed018ab8fd7c8b9e677
Abdu-Salam Kurbanov <aburib1iya@gmail.com>
2023-12-12 01:23:34 +0400

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', loadComponent: () => import('./core/auth/login/login.component').then(m =>m.LoginComponent)},
+    {path: 'register', loadComponent: () => import('./core/auth/register/register.component').then(m =>m.RegisterComponent)},
+];
