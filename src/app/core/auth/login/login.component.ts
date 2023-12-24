d51b7a72064b0dbb53cb941f0dad34ec5dd17d70
Abdu-Salam Kurbanov <aburib1iya@gmail.com>
2023-12-25 01:24:56 +0400

    Rasulbid

--- src/app/core/auth/login/login.component.ts
+++ src/app/core/auth/login/login.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AccountFacadeService } from '../services/account-facade.service';
+import { Router } from '@angular/router';
+import { AccountCredential } from '../model/account.credential';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm : FormGroup
+  
+
+  constructor(private accountFacade: AccountFacadeService, private router: Router){
+    this.loginForm = new FormGroup({
+      password: new FormControl(null , [Validators.required]),
+      email: new FormControl(null , [Validators.required,Validators.email])
+    })
+  }
+  onSubmit(){
+    this.accountFacade.login(this.loginForm.value as AccountCredential)
+    this.router.navigate(["/main"])
+  }
 
 }
---
+++ src/app/core/auth/services/token-interceptor.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, inject } from '@angular/core';
+import { TokenStorageService } from './token-storage.service';
+import { HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenInterceptorService {
+
+  rasul = inject(TokenStorageService)
+
+  constructor(private token_storage : TokenStorageService) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let requestWithToken = this.addAccessToken(request);
+    return next.handle(requestWithToken);
+  }
+
+  addAccessToken(request: HttpRequest<any>) : HttpRequest<any> {
+    if (this.token_storage.accessToken)
+      return request.clone({setHeaders: { 'Authorization': `Bearer ${this.token_storage.accessToken}` }});
+    else
+      return request;
+  }
+}
